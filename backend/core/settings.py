0c381f1d14b671848e549983438d64decd9f4990
MarcoVNDev <marcovndev@gmail.com>
2024-10-18 09:12:59 -0600

    Add API documentation

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'coreapi',
     'apps.authenticate',
-    'coreapi'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+}
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('apps.authenticate.urls'))
+    path('users/', include('apps.authenticate.urls')),
+    path('docs/', include_docs_urls(title="Inventory API")),
 ]
